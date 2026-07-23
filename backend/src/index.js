5bde50f05c58eadec03cfc3fc1ac25abf786adcd
Akshat Nagar <akshatn9414@gmail.com>
2026-07-23 16:05:02 +0530

    debugging render env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
-import app from "./app.js";
 import dotenv from 'dotenv'
-import connectToDB from "./db/index.js";
-
 dotenv.config({
     path: '.env'
 })
+
+import app from "./app.js";
+import connectToDB from "./db/index.js";
+
+console.log("Is PORT injected by Render?:", process.env.PORT);
+console.log("Is MONGO_URI injected by Render?:", !!process.env.MONGO_URI);
 
 connectToDB()
 .then(() => {
